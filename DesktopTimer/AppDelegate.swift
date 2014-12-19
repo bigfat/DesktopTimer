edaf7d120be942ac8b83aac20068abe6cc7996b7
Colton Blume <colton.blume@gmail.com>
2014-12-19 10:28:59 -0600

    Initial Commit

---
+++ DesktopTimer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DesktopTimer
+//
+//  Created by Colton Blume on 12/19/14.
+//  Copyright (c) 2014 Colton Blume. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DesktopTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DesktopTimer
+//
+//  Created by Colton Blume on 12/19/14.
+//  Copyright (c) 2014 Colton Blume. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
